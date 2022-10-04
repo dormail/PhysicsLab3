28974b40e4e28b6f54a01180a013c6b565c8cde9
Matthias Maile <matthias.maile@pm.me>
2022-10-04 21:39:36 +0900

    results added, curve fit in negative regime

--- S4 Photoelectric effect/analysis_own_measurement.py
+++ S4 Photoelectric effect/analysis_own_measurement.py
@@ -4,23 +4,68 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+from uncertainties import ufloat
+from scipy.optimize import curve_fit
 
 # matplotlib setup
 mpl.use('pgf')
 
+# data import
 data = pd.read_csv('data/data.csv')
 U = data['U']
 current = data.to_numpy()[:,1:]
 current_average = np.nanmean(current, axis=1)
 current_std = np.nanstd(current, axis=1)
+current_rel_std = current_std / current_average
+
+
+# output as tex table
+# data frame with all new data and output as tex table
+data.insert(len(data.columns), 'average', current_average)
+data.insert(len(data.columns), 'std', current_std)
+data.insert(len(data.columns), 'rel_std', current_rel_std*100)
+
+# modified line_terminator to include a percentage symbol
+data.to_csv('build/our_data.tex',
+            sep='&',
+            float_format="%.2f",
+            header=False,
+            index=False,
+            decimal='.',
+            line_terminator='\% \\\\\n')
+
+# linear fit for U<0
+def linear(x, m, b):
+    return m*x + b
+
+current_average_red = current_average[:31]
+U_red = U[:31]
+
+popt, pcov = curve_fit(linear, U_red, current_average_red,
+          p0=(3,300))
+m, b = popt
+m_err, b_err = np.sqrt(np.diag(pcov))
+
+m_u = ufloat(m, m_err)
+b_u = ufloat(b, b_err)
+
+print(f'm = {m_u}')
+print(f'b = {b_u}')
 
 # plotting
 plt.clf()
 plt.errorbar(U, current_average, xerr=np.ones_like(U)*.5, yerr=current_std, 
             ls='None',
-            label='Photo current')
+            label='Measurement data',
+             color='k')
+
+plt.plot(U_red, linear(U_red, m, b),
+         c='r',
+         alpha=1,
+         label=rf'linear curve fit for $U<0$')
 
 plt.ylabel('Photo current / pA')
 plt.xlabel(r'Accelerating voltage / V')
 plt.tight_layout()
+plt.legend()
 plt.savefig('build/photocurrent_incorrect.pdf')
