5a208f57d235dbfc25fd554d732ba0781068938a
Matthias Maile <matthias.maile@pm.me>
2022-12-19 08:54:48 +0900

    plot of intensity added

---
+++ O2 Laser/plot.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+mpl.use('pgf')
+
+data = pd.read_csv('intensity.csv')
+
+current = data['Tube Current mA']
+I = data['I'] 
+I = I / np.max(I)
+
+plt.xlabel(r'Laser current / mA')
+plt.ylabel(r'Laser intensity / relative units')
+
+plt.scatter(current, I, marker='+')
+plt.savefig('build/intensity.pdf')
+
